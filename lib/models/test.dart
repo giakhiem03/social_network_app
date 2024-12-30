e999456e1382b652dc8c59b9e12a6f2df394a50e
qhiennn <lqhlehien2909@gmail.com>
2024-12-30 15:57:53 +0700

    b

---
+++ lib/models/test.dart
@@ -0,0 +1,21 @@
+class test {
+  Role(
+      {required this.roleId,
+        this.roleName,
+      });
+
+  final int roleId;
+  final String? roleName;
+
+  factory Role.fromJson(Map<String, dynamic> json) {
+    return Role(
+        roleId: json["roleId"],
+        roleName: json["roleName"]
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "roleId": roleId,
+    "roleName": roleName
+  };
+}
