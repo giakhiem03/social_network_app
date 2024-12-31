44771142f66250a14a7a5e454d21a15494737e5f
XShawn-0703 <sino167200333@gmail.com>
2025-01-01 01:25:02 +0700

    Notification

--- lib/screen/NotificationPage.dart
+++ lib/screen/NotificationPage.dart
@@ -1,95 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:social_network_project/ApiService/ApiService.dart';
 import 'package:social_network_project/models/Notifications.dart';
 
 import '../models/User.dart';
+import 'ProfilePage.dart';
 
 class NotificationPage extends StatefulWidget {
-  final User user;
 
-  NotificationPage({required this.user, Key? key}) : super(key: key);
+  NotificationPage({super.key});
 
   @override
-  State<NotificationPage> createState() => _NotificationPageState();
+  State<NotificationPage> createState() => _NotificationPage();
 }
 
-class _NotificationPageState extends State<NotificationPage> {
-  final ApiService _apiService = ApiService();
+class _NotificationPage extends State<NotificationPage> {
+  late Future<List<Notifications>> futureNote;
+
+  ApiService apiService = ApiService();
+
+  @override
+  void initState() {
+    super.initState();
+    futureNote = apiService.getAllNotes();
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final userProvider = Provider.of<UserProvider>(context); // Lấy dữ liệu người dùng từ Provider
+    final user = userProvider.user;
+
     return Scaffold(
       backgroundColor: Colors.white10,
-      appBar: AppBar(
-        backgroundColor: Colors.white10,
-        title: const Text(
-          'Thông Báo',
-          style: TextStyle(color: Colors.white70, fontSize: 26),
-        ),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: FutureBuilder<List<Notifications>>(
-        future: _apiService.getAllNotes(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-                child: Text(
-                  'Lỗi: ${snapshot.error}',
-                  style: const TextStyle(color: Colors.white70),
-                ));
-          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            final notifications = snapshot.data!
-                .where((note) =>
-            note.userIdReceive.username == widget.user.username)
-                .toList();
-
-            if (notifications.isEmpty) {
-              return const Center(
-                child: Text(
-                  'Không có thông báo nào',
-                  style: TextStyle(color: Colors.white70),
-                ),
-              );
-            }
-
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: notifications.length,
-                itemBuilder: (context, index) {
-                  final notification = notifications[index];
-                  return Card(
-                    color: Colors.blueGrey,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 25,
-                        backgroundImage:
-                        NetworkImage(notification.userIdSend.image!),
-                      ),
-                      title: Text(
-                        notification.userIdSend.fullName!,
-                        style: const TextStyle(color: Colors.white70),
-                      ),
-                      subtitle: Text(
-                        notification.notificationCategory.id == 1
-                            ? '${notification.userIdSend.fullName} đã like bài viết của bạn'
-                            : '${notification.userIdSend.fullName} đã bình luận bài viết của bạn',
-                        style: const TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  );
-                },
+      body: Column(
+        children: [
+          // Tiêu đề
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 60),
+              Text(
+                'Thông Báo',
+                style: TextStyle(color: Colors.white70, fontSize: 26),
               ),
-            );
-          } else {
-            return const Center(
-                child: Text('Không có thông báo nào',
-                    style: TextStyle(color: Colors.white70)));
-          }
-        },
+            ],
+          ),
+          // ListView.builder
+          Expanded(
+            child: FutureBuilder<List<Notifications>>(
+              future: futureNote,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Lỗi: ${snapshot.error}',style: const TextStyle(color: Colors.white70),));
+                } else if (snapshot.hasData || snapshot.data!.isNotEmpty) {
+                  // ListView.builder hiển thị các thông báo
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      Notifications notification = snapshot.data![index];
+                      return notification.userIdReceive.username == user!.username ?
+                      Container(
+                        padding: const EdgeInsets.only(top: 10,bottom: 10),
+                        margin: const EdgeInsets.only(bottom: 10),
+                        color: Colors.blueGrey,
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 18),
+                            CircleAvatar(
+                              radius: 16,
+                              backgroundImage: NetworkImage(notification.userIdSend.image!), // Thêm avatar động từ dữ liệu
+                            ),
+                            const SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  notification.userIdSend.fullName!, // Hiển thị tên người dùng
+                                  style: const TextStyle(color: Colors.white70),
+                                ),
+                                notification.notificationCategory.id == 1 ?
+                                Text( '${notification.userIdSend.fullName} đã like bài viết của bạn',
+                                  style: const TextStyle(color: Colors.white70),
+                                ) :
+                                Text( '${notification.userIdSend.fullName} đã bình luận bài viết của bạn',style: const TextStyle(color: Colors.white70),),
+                              ],
+                            ),
+                          ],
+                        ) ,
+                      ) : Container();
+                    },
+                  );
+                } else {
+                  return const Center(child: Text('Không có thông báo nào',style: TextStyle(color: Colors.white70),));
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
